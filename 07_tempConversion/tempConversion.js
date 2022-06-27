84e7d178ccc3ffae8a3eb6ee532d11b38f686fd7
Polina <polina.voylokov@sciencespo.fr>
2022-06-27 13:48:27 +0200

    first set of exercises done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,13 @@
-import { create, all } from 'mathjs'
-const config = { }
-const math = create(all, config)
 
 const ftoc = function(fahr) {
-return math.round((fahr-32)*5/9,1)
+cels = Math.round((fahr-32)*5/9*10)
+
+  return cels/10
 };
 
 const ctof = function(cels) {
-
-return math.round(cels*9/5+32,1)
+fahr = Math.round((cels*9/5+32)*10)
+return fahr/10
 };
 
 // Do not edit below this line
